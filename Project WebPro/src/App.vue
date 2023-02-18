64f4f51d64a8ddea8fec31ee7a7676d869e232cd
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2023-02-18 20:12:28 +0700

    update almost finish

--- Project WebPro/src/App.vue
+++ Project WebPro/src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import nav from './components/nav.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <nav />
+    <HelloWorld msg="Vite + Vue" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
