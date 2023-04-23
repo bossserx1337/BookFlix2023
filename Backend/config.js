b2c4327cc7b51962201f0090ded95382fed279ad
bosssxandaR <113000499+bosssxandaR@users.noreply.github.com>
2023-04-23 17:47:18 +0700

    updatebook function

--- Backend/config.js
+++ Backend/config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
-  database: 'project',
+  password: 'secret',
+  database: 'mywebpro',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
